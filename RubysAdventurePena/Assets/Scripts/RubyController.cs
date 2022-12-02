b4159e96ff0756e84b41d2f71451f1207776ecb3
MatthewPena1 <724514@student.dusd.net>
2022-12-02 11:43:36 -0800

    Updated Ruby's Adventure
    Fixed Collectibles and Damageables; modified the damageable sprite;
    imported a bot png; created a bot prefab; added rigidbody2d and box collider 2d; created EnemyController script;

--- RubysAdventurePena/Assets/Scripts/RubyController.cs
+++ RubysAdventurePena/Assets/Scripts/RubyController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
    
     public int health { get { return currentHealth; }}
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -26,6 +30,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -39,6 +50,15 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
